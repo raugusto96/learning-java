1c1dd803624f884c02353a2aad64fcfaedf39abb
Rodrigo <rodrigoaugusto96@outlook.com>
2022-06-11 19:55:13 -0300

    Feat: Add exercise 3

---
+++ Workspace/curso_java/src/Exercicio3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1;
+		int num2;
+		int num3;
+		int num4;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite quatro números para o cálculo");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		num4 = sc.nextInt();
+		sc.close();
+		
+		int difference = num1 * num2 - num3 * num4;
+		System.out.println("A diferença é: " + difference);
+	}
+
+}
