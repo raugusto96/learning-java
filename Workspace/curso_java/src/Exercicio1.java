cec609835c5e201cc69831ca1cade65a262e8e97
Rodrigo <rodrigoaugusto96@outlook.com>
2022-06-11 19:40:57 -0300

    Feat: add exercise 1

---
+++ Workspace/curso_java/src/Exercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		int y;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite dois números");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		sc.close();
+		int z = x + y;
+		System.out.println("A soma de " + x + " + " + y + " = " + z);
+	}
+
+}
