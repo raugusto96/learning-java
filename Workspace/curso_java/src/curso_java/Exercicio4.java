49ad6417c4fe31e52b9657d8415df57f93cf26d9
Rodrigo <rodrigoaugusto96@outlook.com>
2022-06-11 20:07:04 -0300

    Feat: Add exercise 4

---
+++ Workspace/curso_java/src/curso_java/Exercicio4.java
@@ -0,0 +1,29 @@
+package curso_java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		int employeeNumber, hours;
+		double salaryPerHour, salary;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o número do empregado");
+		employeeNumber = sc.nextInt();
+		System.out.println("Digite o salário por hora do empregado");
+		salaryPerHour = sc.nextFloat();
+		System.out.println("Digite as horas trabalhadas");
+		hours = sc.nextInt();
+		
+		sc.close();
+		
+		salary = salaryPerHour * hours;
+		System.out.println("O número do empregado é: " + employeeNumber);
+		System.out.printf("O salário do empregado é: U$ %.2f%n", salary);
+	}
+
+}
