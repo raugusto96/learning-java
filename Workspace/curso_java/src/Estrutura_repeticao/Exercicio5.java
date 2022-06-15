d7a8d6408d1cc88badac7b3666685d0d47edcc37
Rodrigo <rodrigoaugusto96@outlook.com>
2022-06-15 15:12:26 -0300

    Feat: Add exercise 5

---
+++ Workspace/curso_java/src/Estrutura_repeticao/Exercicio5.java
@@ -0,0 +1,30 @@
+package Estrutura_repeticao;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number, in = 0, out = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um número");
+		number = sc.nextInt();
+		
+		for (int i = 0; i < number; i++) {
+			System.out.println("Digite um número");
+			int x = sc.nextInt();
+			if (x >= 10 && x <= 20) {
+				in += 1;
+			} else {
+				out += 1;
+			}
+		}
+		
+		System.out.println("in " + in);
+		System.out.println("out " + out);
+		
+	}
+
+}
